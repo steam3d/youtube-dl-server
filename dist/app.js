e729bb92f588099adbe1e48a0e0e3271ff23c4e2
Alexander Maslov <steam3d@gmail.com>
2023-07-07 12:00:05 +0300

    Enable https

--- dist/app.js
+++ dist/app.js
@@ -10,6 +10,15 @@ const compression = require('compression');
 const YoutubeDl_1 = require("./YoutubeDl");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var key  = fs.readFileSync('/etc/letsencrypt/live/justtype.ru/privkey.pem', 'utf8');
+var cert = fs.readFileSync('/etc/letsencrypt/live/justtype.ru/cert.pem', 'utf8');
+var ca = fs.readFileSync('/etc/letsencrypt/live/justtype.ru/chain.pem', 'utf8');
+var credentials = {key: key, cert: cert, ca:ca};
+
 app.use(compression());
 app.use(cors());
 app.get('/v1/video', async (req, res) => {
@@ -61,8 +70,17 @@ app.get('/watch', async (req, res) => {
         res.send(e);
     }
 });
-app.listen(port, () => {
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(port, () => {
     console.log(`server is listening on http://localhost:${port}`);
     console.log(`Try this url in your browser: http://localhost:${port}/watch?v=dQw4w9WgXcQ&cli=yt-dlp`);
+});
+
+httpsServer.listen(8443, () => {
+    console.log(`server is listening on https://localhost:8443`);
+    console.log(`Try this url in your browser: https://localhost:8443/watch?v=dQw4w9WgXcQ&cli=yt-dlp`);
 });
 //# sourceMappingURL=app.js.map
